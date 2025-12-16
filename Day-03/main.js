d8375ab0c20b3b987e6956a0f177cc896afb2a3f
AnujPaudel22 <anuj.paudel2015gmail.com>
2025-12-16 18:01:41 +0545

    Day 3

---
+++ Day-03/main.js
@@ -0,0 +1,32 @@
+// // type of variable
+// // redeclareable updatable work in global scoope
+// var a=10;
+// var a=30;
+// console.log(a);
+
+// // noredeclareable updatable works in block scope
+// let b=20;
+// b=50;
+// console.log(b);
+
+// // no-redeclareable no updateable work in block scope
+// const c=30;
+// console.log(c)
+
+
+// // data types:
+// // primitive: single value like: number string bolean null undefined
+// // non-primitive: array object function
+
+
+// // console.log(typeof null) object output is an error which has not been yet solved
+// console.log(typeof 4)
+
+// // console type
+// // console.log, console.error, console.alert, console.warn
+let a=10;
+let b=10;
+console.log(a+b);
+console.error("This is error");
+console.warn("This is warning");
+console.alert("This is alert")
