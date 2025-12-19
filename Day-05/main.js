2eb12c1b77b3ebaf46579da31c1c13999af6cbf2
AnujPaudel22 <anuj.paudel2015gmail.com>
2025-12-19 17:46:17 +0545

    Day-05 Loop

---
+++ Day-05/main.js
@@ -0,0 +1,137 @@
+// // // If-else
+// // // operators
+// // // Arithemetic operator
+
+// // // let a= 110;
+// // // let b=3;
+
+// // // console.log(a+b)
+// // // console.log(a-b)
+// // // console.log(a*b)
+// // // console.log(a/b)
+// // // console.log(a%b)
+// // // console.log(10**3)
+
+// // // Assignment operator
+// // // let a=10;
+// // // // a+=3
+// // // a*=3
+// // // a++
+// // // console.log(a)
+
+// // // Comparision operators => == === != !== < > <= >=
+
+// // // console.log(5!="5")
+
+// // // logical operator && || !
+// // // && if both condiytion are true output is true
+// // // || if one condition is true then output is true
+// // // ! it is inverse
+
+// // // ternary operator
+
+// // // let age=17
+// // // let canDrive=age>=18 ? "yes":"no";
+// // // console.log(canDrive)
+
+// // let age =20;
+// // let car=true;
+// // let license=true;
+// // if(age<16){
+// //     console.log("too young to drive");
+// // }
+// // else if (age>=16 && age<=18){
+// //     console.log("Can apply for driving license");
+// // }
+
+// // else if(age>18){
+// //     if (license && car){
+// //         console.log("Can drive your own car");
+// //     }
+// //     else if(license && car){
+// //         console.log("Can drive but does not own a car");
+// //     }
+// //     else if(license && car){
+// //         console.log("Owns a car but can not drive");
+
+// //     }
+// //     else{
+// //         console.log("Needs license first");
+// //     }
+
+
+// // }
+// // else{
+// //     console.log("Invalid output")
+// // }
+
+// // Nested if else
+// // let mark=85;
+// // if(mark>=40){
+// //     if (mark>=90){
+// //         console.log("Excellent");
+// //     }
+// //     else{
+// //         console.log("Pass")
+// //     }
+// // }
+// // else{
+// //     console.log("Fail");
+// // }
+
+// // Switch-Case
+
+// let food="pizza";
+// switch(food){
+//     case "Burger":
+//         console.log("Price:$7");
+//         break;
+//     case "pasta":
+//         console.log("Price:$9");
+//         break;
+
+//     case "pizza":
+//         console.log("Price:$8");
+//         break;
+
+//     case "salad":
+//         console.log("Price:$4");
+//         break;
+//     case "omlete":
+//         console.log("Price:$5");
+//         break;
+//     case "Chowmein":
+//         console.log("Price:$10");
+//         break;
+//     default:
+//         console.log("Invalid")
+
+// }
+
+// Loops
+
+// For loop
+// for(i=0;i<=5;i++){
+//     console.log(i);
+// }
+// while
+// let i=0;
+// while (i<=5){
+//     console.log(i);
+//     i++;
+// }
+// Do...While
+let i=0;
+do{
+    console.log(i);
+    i++;
+}
+while(i<=5)
+
+// For in loop
+// Nested loop
+// Break Statement
+
+// For loop
+
+   
