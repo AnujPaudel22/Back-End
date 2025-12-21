058c1ff963cbd99445754f2021d852c2c2ab02f9
AnujPaudel22 <anuj.paudel2015gmail.com>
2025-12-21 17:55:03 +0545

    Day 7 function continue breaks

---
+++ Day-07/main.js
@@ -0,0 +1,67 @@
+// for in loop
+// let number=[10,20,30];
+
+// for (let index in number){
+//     console.log(index, number[index])
+
+// }
+
+// const studentMarks={
+//     ram:45,
+//     shyam:90,
+//     hari:85,
+//     sita:67
+
+// }
+
+// for (let name in studentMarks){
+//     if (studentMarks[name]<40){
+//         continue;
+//     }
+//     console.log(`${name} passed with ${studentMarks[name]} marks`);
+//     if(studentMarks[name]>=90){
+//         console.log(`${name} top student found`);
+//     }
+// }
+
+// Continue
+// for(let i=0;i<=5;i++){
+//     if(i===3){
+//         continue;
+//     }
+//     console.log(i)
+// }
+
+// break
+
+// for(let i=0;i<=5;i++){
+//     if(i===3){
+//         break;
+//     }
+//     console.log(i)
+// }
+
+// Nested loop
+// for(i=1;i<=5;i++){
+//     for(i=1;j<=6;j++){
+//         console.log(i,j);
+//     }
+
+
+// }
+
+// Functions
+// parameter
+// argument
+// return
+
+// function add(a,b){
+//     return a+b
+// }
+// let sum=add(5,6)
+// console.log(sum)
+
+// const fruits=["apple","Banana","Orange"]
+// fruits.forEach((fruit,i)=>{
+//     console.log(i+1,fruit)
+// })
